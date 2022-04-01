6228f3dc764c426c7538e1ab7abfbb558886f18f
jaesungbae <bjs2279@gmail.com>
2022-04-01 21:26:44 +0900

    re-initialize repository

---
+++ main.py
@@ -0,0 +1,12 @@
+import flask
+from flask import Flask, render_template, send_from_directory, send_file
+from flask.views import MethodView
+
+app = Flask(__name__)
+
+class MainView(MethodView):
+    def get(self):
+        return flask.render_template("index.html")
+
+
+        
